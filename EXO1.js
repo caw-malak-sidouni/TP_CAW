1c73440ab9e34ffb91b7362b767c768dbff1ca16
caw-malak-sidouni <115424482+caw-malak-sidouni@users.noreply.github.com>
2022-10-11 19:54:04 +0200

    Update EXO1.js

--- EXO1.js
+++ EXO1.js
@@ -4,10 +4,10 @@ var boundaries = document.querySelectorAll(".boundary");
 var s_point = document.querySelector("#start");
 var playing = true ;
 
-s_point.addEventListener("mouseover", StartGame);
-function StartGame() {
+s_point.addEventListener("mouseover", StartPlaying);
+function StartPlaying() {
   document.getElementById("status").innerHTML =
-    "Move your mouse over the \"S\" to begin";
+    "Move your mouse over the \"S\" to start";
   for (var i = 0; i < boundaries.length; i++) {
     boundaries[i].addEventListener("mouseover", BoundiesCheck);
   }
