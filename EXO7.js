f1a5f255b7bce47dc209c483d003e50f76502347
caw-malak-sidouni <115424482+caw-malak-sidouni@users.noreply.github.com>
2022-10-11 21:15:04 +0200

    Update EXO7.js

--- EXO7.js
+++ EXO7.js
@@ -22,61 +22,18 @@ function boundriesCheck() {
   if (playing == false) {
     return;
   }
-  document.getElementById("status").innerHTML = "YOU LOSE!";
+  document.getElementById("status").innerHTML = "YOU LOST!";
   s_point.removeEventListener("mouseover", StartGame);
   playing = false;
   for (var i = 0; i < boundaries.length; i++) {
     boundaries[i].style.backgroundColor = "red";
   }
-  //exercice6
-//On-Page status updates
-var boundaries = document.querySelectorAll(".boundary");
-var s_point = document.querySelector("#start");
-var e_point = document.querySelector("#end");
-var status = document.querySelector("#status");
-var playing = false;
-
-s_point.addEventListener("mouseover", StartGame);
-s_point.addEventListener("click", resetGame);
-e_point.addEventListener("mouseover", e_point_listener);
-
-function StartGame() {
-  playing = true;
-  document.getElementById("status").innerHTML = "Move your mouse over the S to begin";
-  for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].addEventListener("mouseover", boundriesCheck);
-  }
-}
-function boundriesCheck() {
-  if (playing == false) {
-    return;
-  }
-  document.getElementById("status").innerHTML = "YOU LOSE!"
-  playing = false;
-  for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].style.backgroundColor = "red";
-  }
-    document.getElementById("status").innerHTML = "click to s to restart the game";
-}
-function e_point_listener() {
-  if (playing == true) {
-    document.getElementById("status").innerHTML = "YOU WIN!"
-  }
-  
-}
-function resetGame() {
-  playing = true;
-  for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].style.backgroundColor = "#eeeeee";
-  }
-  document.getElementById("status").innerHTML = "Move your mouse over the S to begin";
-}
 }
 function e_point_listener() {
   if (playing == true) {
     s_point.removeEventListener("mouseover", StartGame);
     s_point.removeEventListener("mouseover", checkCheating);
-    document.getElementById("status").innerHTML = "YOU WIN!";
+    document.getElementById("status").innerHTML = "YOU WON!";
   }
 
   playing = false;
